4a180646bf708b45f377fb45b299a4f12aaf23d8
User <asqarservis0000@gmail.com>
2025-08-15 15:43:21 +0500

    updated

--- db/model.py
+++ db/model.py
@@ -1,15 +1,13 @@
+from sqlalchemy import String, BIGINT
+from sqlalchemy.orm import Mapped, mapped_column
 
-from sqlalchemy import Text, String, BIGINT, DECIMAL, ForeignKey, Integer, Float
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from db import Base, db
+from db import Base,db
 from db.utils import CreatedModel
 
 
-
 class User(CreatedModel):
-    user_id:Mapped[int]=mapped_column(BIGINT,unique=True)
-    username:Mapped[str]=mapped_column(String,nullable=True)
-
+    user_id: Mapped[int] = mapped_column(BIGINT, unique=True)
+    username: Mapped[str] = mapped_column(String, nullable=True)
 
 
 metadata = Base.metadata
